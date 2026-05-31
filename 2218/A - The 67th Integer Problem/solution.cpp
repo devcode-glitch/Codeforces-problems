e6c568d01db1aa43a4121e4488bc3cf03d2a5e58
Devanathan CS <learnerdeva123@gmail.com>
2026-05-31 06:21:30 +0530

    Add The 67th Integer Problem [A] from Codeforces

---
+++ 2218/A - The 67th Integer Problem/solution.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+using ll = long long;
+ /* PROBLEM IS SIMPLE, JUST OBSERVE CAREFULLY
+    YOU CAN DO THIS MATE!, THINK LIKE A PROBLEM SOLVER*/
+void solve(){
+  int n;cin>>n;
+  if(n == -67) cout<<-67<<endl;
+  else if(n == 67) cout<<67<<endl;
+  else cout<<++n<<endl;         
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    int t = 1;
+    cin>>t;
+    while(t--){
+       solve();
+    }
+}  
