4bf4fef766fbf1d3c2fa4e3da2d15242f29b962b
Devanathan CS <learnerdeva123@gmail.com>
2026-05-31 06:20:30 +0530

    Add A Simple Sequence [A] from Codeforces

---
+++ 2210/A - A Simple Sequence/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+using ll = long long;
+ /* PROBLEM IS SIMPLE, JUST OBSERVE CAREFULLY
+    YOU CAN DO THIS MATE!, THINK LIKE A PROBLEM SOLVER*/
+void solve(){
+  int n;
+  cin>>n;
+  for(int i=n;i>=1;i--){
+    cout<<i<<" ";
+  }
+  cout<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    int t = 1;
+    cin>>t;
+    while(t--){
+       solve();
+    }
+}  
