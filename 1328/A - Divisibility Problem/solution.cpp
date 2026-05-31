d14a9102bdba59925a0478e987e2e1aae4d43d77
Devanathan CS <learnerdeva123@gmail.com>
2026-05-31 06:17:40 +0530

    Add Divisibility Problem [A] from Codeforces

---
+++ 1328/A - Divisibility Problem/solution.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    for(int i =0;i<t;i++){
+        int a,b;
+        cin>>a>>b;
+        int rem = a%b;
+        int steps = (b-rem)%b;
+        cout<<steps<<endl;
+    }
+}
