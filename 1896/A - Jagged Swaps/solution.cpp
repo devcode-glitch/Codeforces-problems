a64317d506826489d0b256d119d8dc4c4894839a
Devanathan CS <learnerdeva123@gmail.com>
2026-05-31 06:21:19 +0530

    Add Jagged Swaps [A] from Codeforces

---
+++ 1896/A - Jagged Swaps/solution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define endl '
+'
+using namespace std;
+using ll = long long;
+ /* PROBLEM IS SIMPLE, JUST OBSERVE CAREFULLY
+    YOU CAN DO THIS MATE!, THINK LIKE A PROBLEM SOLVER*/
+void solve(){
+  int n;
+  cin>>n;
+  vector<int> arr(n);
+  for(int i=0;i<n;i++) cin>>arr[i];
+  cout<<((arr[0] == 1)? "YES" : "NO")<<endl;
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); 
+    int t = 1;
+    cin>>t;
+    while(t--){
+       solve();
+    }
+}  
