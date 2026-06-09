7e965a3af20c5b0634ee7d44b6596baff4ae6c63
Devanathan CS <learnerdeva123@gmail.com>
2026-06-09 06:51:35 +0530

    Add Euclid, Sequence and Two Numbers [A] from Codeforces

---
+++ 2234/A - Euclid, Sequence and Two Numbers/solution.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fio() ios::sync_with_stdio(false);cin.tie(nullptr)
+using i64 = long long;
+#define endl "
+"
+#define yes cout<<"YES
+"
+#define no cout<<"NO
+"
+ 
+/*
+GOLDEN RULES
+PROBLEMS ARE SIMPLE
+PROOFS ARE SIMPLE
+IMPLEMENTATION IS SIMPLE
+ 
+SIMPLIFY THE PROBLEM
+DIVIDE INTO SMALL WHENEVER POSSIBLE
+*/
+ 
+/*
+  Simple observations 
+*/
+int main(){
+  fio();
+  int t = 1;
+  cin>>t;
+  while(t--){
+    int n;
+    int ct = 0;
+    cin>>n;
+    vector<int> arr(n),mark(n,0);
+    for(int i =0;i<n;i++) cin>>arr[i];
+    sort(arr.rbegin(),arr.rend());
+    if(n == 2){for(auto itr : arr) {cout<<itr<<" ";}cout<<endl;}
+    else{
+        bool found = true;
+        for(int i = 0;i+2<n;i++){
+            i64 x = arr[i];
+            i64 y = arr[i+1];
+            if(x%y == arr[i+2]){continue;}
+            else{found = false;cout<<-1<<endl;break;}
+        }
+        if(found) cout<<arr[0]<<" "<<arr[1]<<endl;
+    }
+    }
+  }
