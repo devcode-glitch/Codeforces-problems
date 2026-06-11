18da014fb5c5e70e5392117495c5bc9867a3e7f4
Devanathan CS <learnerdeva123@gmail.com>
2026-06-11 20:16:17 +0530

    Add Omkar and Last Class of Math [B] from Codeforces

--- 1372/B - Omkar and Last Class of Math/solution.cpp
+++ 1372/B - Omkar and Last Class of Math/solution.cpp
@@ -1,43 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-using i64 = long long;
+using ll = long long;
 #define endl "
 "
-#define yes cout<<"YES
-"
-#define no cout<<"NO
-"
- 
-/*
-GOLDEN RULES
-SOLUTIONS ARE SIMPLE
-PROOFS ARE SIMPLE
-IMPLEMENTATION IS SIMPLE
- 
-SIMPLIFY THE PROBLEM
-DIVIDE INTO SMALL WHENEVER POSSIBLE
-*/
- 
-/*
-  Simple observations  
-  we are given a number n
-  we have to find a and b a+b == n such that lcm(a,b) is minimised
-  n<= 1e9
- 
-  when is lcm minimised ?
- 
-*/
- 
 void solve(){
-  i64 n;
-  cin>>n;
-  for(int i=2;i*i<=n;i++){
+   ll n;
+   cin>>n;
+   for(int i = 2;i*i<=n;i++){
     if(n%i == 0){
-        cout<<n/i<<" "<<n-(n/i)<<endl;
+        cout<<n/i<<" "<<(n - n/i)<<endl;
         return;
     }
-  }
-  cout<<1<<" "<<n-1<<endl;
+   }
+   cout<<1<<" "<<n-1<<endl;
 }
 int main(){
   ios::sync_with_stdio(false);
@@ -45,5 +20,4 @@ int main(){
   int t = 1;
   cin>>t;
   while(t--) solve();
-  return 0;
 }
