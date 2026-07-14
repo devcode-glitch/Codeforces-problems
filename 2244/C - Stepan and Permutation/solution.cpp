79efa44c94093bf5a208f1e2b18e3784d3792301
Devanathan CS <learnerdeva123@gmail.com>
2026-07-14 22:31:19 +0530

    Add Stepan and Permutation [C] from Codeforces

---
+++ 2244/C - Stepan and Permutation/solution.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+using i64 = long long;
+using u64 = unsigned long long; //maximum limit is 2^64 - 1, positive only
+using l64 = long double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+#define decimal(x,n) fixed<<setprecision(n)<<x /* output x precision n   */
+#define nl "
+"
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define yes cout<<"YES
+"
+#define no cout<<"NO
+"
+#define fio ios::sync_with_stdio(false),cin.tie(nullptr)
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
+long long pMOD(long long a, long long b, long long mod = 998244353) {long long res = 1;a %= mod;while (b > 0) {if (b & 1) res = res * a % mod;a = a * a % mod;b >>= 1;}return res;}
+long long power(long long a, long long b) {long long res = 1;while (b > 0) {if (b & 1)res = res * a;a = a * a;b >>= 1;}return res;}
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ GOLDEN RULES
+SOLUTIONS ARE SIMPLE
+PROOFS ARE SIMPLE
+IMPLEMENTATION IS SIMPLE
+YOU'RE DOING A CP PROBLEM NOT A HEURISITC, A SOLUTION ALREADY EXISTS
+ALWAYS THINK IF I CAN SOLVE IT , HOW WOULD IT BE ??
+SIMPLE OBSERVATIONS
+p[i] is the ith element
+i is its desired/required index
+ 
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// C. Stepan and Permutation
+// https://codeforces.com/contest/2244/problem/C
+*/
+void solve(){
+    int n,x,y;
+    cin>>n>>x>>y;
+    vector<i64> p(n+1);
+    for(int i=1;i<=n;++i) cin>>p[i];
+    int g = gcd(x,y);
+    for(int i=1;i<=n;++i){
+        if((p[i] - i)%g != 0){
+            cout<<"NO
+";
+            return;
+        }
+    }
+    cout<<"YES
+";
+ 
+}
+int main(){
+#ifdef LOCAL
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    freopen("error.txt","w",stderr);
+#endif
+    fio;int t=1;cin>>t;
+while(t--) {dbg(" #########################################################  ");solve();}
+return 0;}
