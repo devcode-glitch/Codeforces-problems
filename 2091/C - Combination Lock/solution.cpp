106efe240ce6a429ca4a57ff472f7034a297d9a4
Devanathan CS <learnerdeva123@gmail.com>
2026-08-19 22:02:44 +0530

    Add Combination Lock [C] from Codeforces

---
+++ 2091/C - Combination Lock/solution.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+using u64 = unsigned long long; //maximum limit is 2^64 - 1, positive only
+using l64 = long double;
+using i128 = __int128_t;
+using u128 = __uint128_t;
+#define decimal(x,n) fixed<<setprecision(n)<<x /* output x precision n   */
+#define nl "
+"
+#define imx INT_MAX
+#define imn INT_MIN
+#define llmx LLONG_MAX
+#define llmn LLONG_MIN
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define yes cout<<"YES
+"
+#define no cout<<"NO
+"
+#define fio ios::sync_with_stdio(false),cin.tie(nullptr)
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define dbg(...)
+#endif
+long long pMOD(long long a, long long b, long long mod = 998244353) {long long res = 1;a %= mod;while (b > 0) {if (b & 1) res = res * a % mod;a = a * a % mod;b >>= 1;}return res;}
+long long power(long long a, long long b) {long long res = 1;while (b > 0) {if (b & 1)res = res * a;a = a * a;b >>= 1;}return res;}
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ GOLDEN RULES
+ 
+SOLUTIONS ARE SIMPLE
+ 
+PROOFS ARE SIMPLE
+ 
+IMPLEMENTATION IS SIMPLE
+ 
+ 
+YOU'RE DOING A CP PROBLEM NOT A HEURISITC, A SOLUTION ALREADY EXISTS
+ 
+ 
+ 
+ALWAYS THINK IF I CAN SOLVE IT , HOW WOULD IT BE ??
+ 
+ 
+ 
+SIMPLE OBSERVATIONS
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+ 
+ 
+ 
+               C. Combination Lock
+2000
+256
+https://codeforces.com/problemset/problem/2091/C
+*/ 
+void solve(){
+    int n;
+    cin>>n;
+    if(!(n&1)){
+        cout<<-1<<nl;
+        return;
+    }
+    else{
+        while(n) cout<<n--<<" ";
+    }
+    cout<<nl;
+    return;
+ 
+}
+int main(){
+int tests = 0;
+    fio;int t=1;cin>>t;
+while(t--) {dbg(++tests);solve();}
+return 0;}
